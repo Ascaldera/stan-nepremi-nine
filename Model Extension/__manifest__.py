1ea4723a35292fb82becbf0e377d1856cb98749d
sekic <luka.sekulic@gmail.com>
2019-09-17 09:58:31 +0000

    expand_models

---
+++ Model Extension/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Model Extension',
+    'summary': 'Extension of Inventory and Contacts',
+    'version': '12',
+    'category': 'Project',
+    'author': 'Luka Sekulic',
+    'license': 'AGPL-3',
+    'depends': [
+        'contacts',
+        'product'
+    ],
+    'data': [
+        'views/expand_models.xml'
+    ],
+    'installable': True,
+}
---
+++ Model Extension/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,1 @@
+from . import expand_contacts_inventory
