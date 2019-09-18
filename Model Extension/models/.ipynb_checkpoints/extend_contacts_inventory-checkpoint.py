6be292fb29b073045c64fe1d7d831097fc23ee93
Luka Sekulic <luka.sekulic@gmail.com>
2019-09-18 13:50:34 +0000

    Cleanup, Add fields

--- Model Extension/models/.ipynb_checkpoints/extend_contacts_inventory-checkpoint.py
+++ Model Extension/models/.ipynb_checkpoints/extend_contacts_inventory-checkpoint.py
@@ -6,17 +6,15 @@ from odoo import fields, api, models, tools
 class ExtendContacts(models.Model):
     _inherit = ['res.partner']
     
-    product_id = fields.Many2one('product.product', string='Kaj Prodaja')
-    product_kraj = fields.One2many('product.product', 'product_id', string='Kdo Prodaja')
-    product_cena = fields.One2many('product.product', 'product_id', string='Cena')
-    
-    tip_stranke = fields.Selection(string='Tip Stranke', selection=[('prodajalec', 'Prodajalec'), , 
-                                                                    ('kupec', 'Kupec'), 
-                                                                    ('najemodajalec', 'Najemodajalec'), 
-                                                                    ('najemnik', 'Najemnik')])
+    tip_stranke = fields.Selection(selection=[('prodajalec', 'Prodajalec'),
+                                              ('kupec', 'Kupec'),
+                                              ('najemodajalec', 'Najemodajalec'),
+                                              ('najemnik', 'Najemnik')])
+    nepremicnine = fields.One2many(comodel_name='product.template', inverse_name="contact", string='Product ID')
+    pridobitev = fields.Char()
     
     
-"""class ExtendInventory(models.Model):
+class ExtendInventory(models.Model):
     _inherit = 'product.template'
     
-    test = fields.Char(string="Test")"""
+    contact=fields.Many2one(comodel_name="res.partner", string="Contact")
