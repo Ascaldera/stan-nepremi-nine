26984afd4288425e117e444089b5855ab6b0023c
Luka Sekulic <luka.sekulic@gmail.com>
2019-09-18 06:51:35 +0000

    Cleanup

--- Model Extension/models/extend_contacts_inventory.py
+++ Model Extension/models/extend_contacts_inventory.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, tools
+from odoo import fields, api, models, tools
 
 
 class ExtendContacts(models.Model):
     _inherit = 'res.partner'
-    _description = "Extend Contact"
     
-    test = fields.Char(string="Test")
+    test = fields.Char(string = 'Test')
+    tip_stranke = fields.Char(string = 'Tip Stranke')
     
     
 class ExtendInventory(models.Model):
     _inherit = 'product.template'
-    _description = "Extend Inventory"
     
     test = fields.Char(string="Test")
