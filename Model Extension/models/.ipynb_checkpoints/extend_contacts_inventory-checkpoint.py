50be8004e068c679e0ddcd8089b33b780f45d5a2
Luka Sekulic <luka.sekulic@gmail.com>
2019-09-20 12:36:57 +0000

    v.1.0 - Contacts

--- Model Extension/models/.ipynb_checkpoints/extend_contacts_inventory-checkpoint.py
+++ Model Extension/models/.ipynb_checkpoints/extend_contacts_inventory-checkpoint.py
@@ -13,8 +13,6 @@ class ExtendContacts(models.Model):
                                               ('najemnik', 'Najemnik')])
     spol = fields.Selection(selection=[('moski', 'Moški'),('ženski', 'Ženski')])
     tretja_oseba = fields.Boolean(string = 'Tretja Oseba')
-    #TODO: potrebno definirati novo tag formo za tag potrebe, nato spodnji field pretvoriti v many2many tag
-    osebnostne_lastnosti = fields.Char(string = 'Osebnostne lastnosti')
     
     #PODATKI O NEPREMIČNINAH
     nepremicnine = fields.One2many(comodel_name='product.template', inverse_name="contact", string='Product ID')
@@ -23,21 +21,24 @@ class ExtendContacts(models.Model):
     objava_kje_datum = fields.Date(string = 'Oglas / kdaj')
     objava_odziv = fields.Char(string = 'Oglas / odziv')
     objava_spremembe = fields.Char(string = 'Oglas / spremembe')
-    objava_stevilo_ogledov = fields.Float(string = 'Oglas / število ogledov')
+    objava_stevilo_ogledov = fields.Integer(string = 'Oglas / število ogledov')
     stranka_odziv = fields.Char(string = 'Ogled / odziv stranke')
     povprasevanje = fields.Char(string = 'Zadnji klici, povpraševanja')
     mnenje = fields.Char(string = 'Naše mnenje')
     
     #PODATKI O ISKANJU
-    poizvedba_nepremicnina = fields.Char(string = 'Kaj išče') #kasneje tag
-    poizvedba_kraj = fields.Char(string = 'Kje išče')
+    #poizvedba_nepremicnina = fields.Char(string = 'Kaj išče')
+    #poizvedba_kraj = fields.Char(string = 'Kje išče')
+    #poizvedba_namen = fields.Char(string = 'Namen')
     poizvedba_cena = fields.Float(string = 'Cena')
-    poizvedba_namen = fields.Char(string = 'Namen')
     poizvedba_kontakt_dan = fields.Date(string = 'Nazadnje kontaktiral')
     poizvedba_zgodovina = fields.Char(string = 'Že ogledano')
     poizvedba_ok = fields.Char(string = 'Kaj jim je bilo všeč')
     poizvedba_nok = fields.Char(string = 'Kaj jim ni bilo všeč')
     poizvedba_kontaktiran = fields.Date(string = 'Nazadnje kontaktiran')
+    poizvedba_balkon = fields.Boolean(string = 'Balkon')
+    poizvedba_dvigalo = fields.Boolean(string = 'Dvigalo')
+    poizvedba_vrt = fields.Boolean(string = 'Vrt')
     
     #PODATKI O PRODAJALCIH
     #create_uid - kdo je vnesel prodajalca
@@ -49,14 +50,13 @@ class ExtendContacts(models.Model):
     prodajalec_komunikacija = fields.Char(string = 'Način komunikacije')
     
     #PODATKI O KUPCIH
+    #kupec_obvescanje = fields.Char(string = 'Obveščanje za ponudbe')
     kupec_naronik = fields.Boolean(string = 'Naročnik')
     kupec_aktiven = fields.Boolean(string = 'Aktivni kupec')
-    kupec_znizana_cena = fields.Boolean(string = 'Znižana cena po oddani ponudbi')
-    kupec_obvescanje = fields.Char(string = 'Obveščanje za ponudbe')
-    
+    kupec_znizana_cena = fields.Boolean(string = 'Znižana cena po oddani ponudbi') 
     
     #PODATKI O NAJEMODAJALCIH
-    najemodajalec_komu = fields.Char(string = 'Komu oddajajo')
+    #najemodajalec_komu = fields.Char(string = 'Komu oddajajo')
     najemodajalec_od = fields.Date(string = 'Od kdaj oddaja')
     najemodajalec_do = fields.Date(string = 'Do kdaj oddaja')
     najemodajalec_otroci = fields.Boolean(string = 'Otroci')
@@ -71,9 +71,157 @@ class ExtendContacts(models.Model):
     najemnik_zaposlitev = fields.Char(string = 'Zaposlitev')
     najemnik_studenti = fields.Boolean(string = 'Študent')
     najemnik_zivali = fields.Boolean(string = 'Živali')
+    
+    contact_tags = fields.Many2many('res.partner.category','res_partner_res_partner_category_rel','category_id','partner_id','Contact Tags')
         
     
 class ExtendInventory(models.Model):
     _inherit = 'product.template'
     
-    contact=fields.Many2one(comodel_name="res.partner", string="Contact")
+    contact = fields.Many2one(comodel_name="res.partner", string="Kdo prodaja")
+    nepremicnina_povrsina = fields.Float(string='Neto površina')
+    nepremicnina_zemljisce_pod = fields.Float(string = 'Zemljišče pod stavbo')
+    nepremicnina_velikost = fields.Float(string = 'Velikost skupaj')
+    nepremicnina_cena_min = fields.Float(string = 'Minimalna cena')
+    nepremicnina_cena_dolgorocno = fields.Float(string = 'Cena / dolgoročni najem')
+    nepremicnina_vrsta = fields.Selection(string = 'Vrsta nepremičnine', selection = [('stanovanje', 'Stanovanje'), 
+                                                                                      ('hisa', 'Hiša'), 
+                                                                                      ('poslovni', 'Poslovni prostor'), 
+                                                                                      ('garaza', 'Garaža'), 
+                                                                                      ('drugo', 'Drugo')])
+    nepremicnina_vrsta_tip_drugo = fields.Char(string = 'Drugo')
+    nepremicnina_vrsta = fields.Selection(string = 'Tip nepremičnine', selection = [('apartma','Apartma'),('soba','Soba'), 
+                                                                                    ('garsonjera', 'Garsonjera'), 
+                                                                                    ('soba1','1-sobno'), 
+                                                                                    ('soba1_5', '1,5-sobno'), 
+                                                                                    ('soba2','2-sobno'), 
+                                                                                    ('soba2_5','2,5-sobno'), 
+                                                                                    ('soba3','3-sobno'), 
+                                                                                    ('soba3_5','3,5-sobno'), 
+                                                                                    ('soba4','4-sobno'), 
+                                                                                    ('soba4_5','4,5-sobno'), 
+                                                                                    ('soba5','5 in večsobno'), 
+                                                                                    ('drugo','Drugo')])
+    nepremicnina_drzava = fields.Char(string = 'Država')
+    nepremicnina_regija = fields.Char(string = 'Regija')
+    nepremicnina_obcina = fields.Char(string = 'Občina')
+    nepremicnina_soseska = fields.Char(string = 'Soseska')
+    nepremicnina_lokacija = fields.Char(string = 'Točna Lokacija')
+    nepremicnina_lokacija_opombe = fields.Char(string = 'Lokacija/opombe')
+    nepremicnina_nadstropje = fields.Integer(string = 'Nadstropje')
+    nepremicnina_st_nadstropij = fields.Integer(string = 'Št. nadstropij')
+    nepremicnina_razlog_prodaje = fields.Char(string = 'Razlog za prodajo')
+    nepremicnina_razlog_oddala = fields.Char(string = 'Razlog zakaj se ni oddala')
+    
+class ExtendContactTags(models.Model):   
+    _inherit = 'res.partner.category'
+    
+    tag_type = fields.Selection(string='Type', selection=[('nepremicnina','Nepremičnina'), 
+                                                          ('lokacija','Lokacija'),
+                                                          ('namen', 'Namen')
+                                                          ('ponudba','Ponudba'), 
+                                                          ('stranka', 'Stranka'), 
+                                                          ('lastnosti','Osebnostne lastnosti')])
+    color = fields.Integer(string='Color Index')
+     
+
+
+"""
+Zakaj se ni oddala
+opis nepremičnine
+leto izgradnje
+leto adaptacije
+kaj je bilo obnovljeno
+streha vrsta kritine
+streha leto obnove/kritja
+fasada/izolacija
+leto obnove fasade
+okna tip
+leto obnove/inštalacije oken
+materiali
+stroški vzdrževanja
+stroški rezervega sklada
+upravnik
+energetska izkaznica
+energijski razred
+Vrsta ogrevanja:
+plinovod
+toplovod
+toplotna črpalka
+biomasa
+Dodatki:
+balkon velikost
+atrij
+klet
+dvigalo
+ogrevanje
+višina stroškov
+lega
+sanitarije
+dnevne sobe
+spalnice
+bazen
+kamin
+vrt
+razgled
+penthouse
+hišni ljubljenčki
+shramba
+klet
+opremljeno
+dostop za invalide
+pogled na
+primerno za študente
+Parkiranje
+garaža
+parkino mesto
+parkirna hiša
+klima
+alarm
+varovan objekt
+video nadzor
+domofon
+360 view ID
+infrastruktura 
+vrtec
+osnovna šola
+fakultete
+pošta 
+trgovina
+banka
+avtocesta
+avtobus
+vlak
+igrišče
+park
+zdravstveni dom
+Komunikacijski priključki
+telefon
+kabel
+optika
+internet
+Lastništvo
+lastništvo pridobljeno na podlagi:
+Dne:
+Plombe:
+Hipoteke:
+solastnina
+prepoved odtujitve/obremenitve
+predkupna pravica
+parcelne številke:
+omejitev po ZKZ
+Predkupne pravice:
+potrdilo o namenski rabi/lokacijska info
+predkupna pravica občine oz drugega upra Vičenca
+ARSO soglasje
+Varstvo naravne ali kulturne dediščine
+Mestni priključek
+GURS vrednost
+Seznam opreme, ki je vključena v ceno
+kuhinja
+dnevna soba
+spalnica
+otroška soba
+hodnik
+kopalnica
+"""
