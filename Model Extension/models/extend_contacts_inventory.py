cd57341ad47f5fd1ed5729df02d62b8dd9003943
Ascaldera <admin@ascaldera.com>
2020-02-07 09:44:45 +0000

    v8.8

--- Model Extension/models/extend_contacts_inventory.py
+++ Model Extension/models/extend_contacts_inventory.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
  
-from odoo import fields, api, models, tools
+from odoo import fields, api, models, tools 
 from odoo import exceptions
 from datetime import date
 from datetime import timedelta
